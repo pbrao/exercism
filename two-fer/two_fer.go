3fca1557b8a08694d634e226566239e2201cf415
Prashanth Rao <pbrao@pmbrao.com>
2019-01-13 14:00:48 -0600

    Better solution without import fmt

--- two-fer/two_fer.go
+++ two-fer/two_fer.go
@@ -1,16 +1,16 @@
-// Package twofer tests out conditions and string interpolation 
+// Package twofer tests out conditions and string interpolation
 package twofer
-
-import "fmt"
 
 // ShareWith  will return a conditional string based on input
 func ShareWith(name string) string {
-	var returnName string	
-	if name == "" { 
+	var returnName string
+	var response string
+	if name == "" {
 		returnName = "you"
 	} else {
 		returnName = name
 	}
-	response := fmt.Sprintf("One for %s, one for me.", returnName)
+	//  response := fmt.Sprintf("One for %s, one for me.", returnName)
+	response = "One for " + returnName + ", one for me."
 	return response
 }
