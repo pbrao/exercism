e11da8675e26c4fac73bedbf126c343960edd2a7
Prashanth Rao <pbrao@pmbrao.com>
2019-02-08 22:43:06 -0600

    Code and performance improvements

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -7,19 +7,17 @@ import (
 // IsIsogram determines if the word is an isogram (i.e. has duplicate characters) or not
 func IsIsogram(word string) bool {
 
-	counter := make(map[rune]int)
-	if word == "" {
-		return true
-	}
+	var seen = map[rune]bool{}
 	for _, letter := range word {
 		if !unicode.IsLetter(letter) {
 			continue
 		}
 		lowerLetter := unicode.ToLower(letter)
-		counter[lowerLetter] = counter[lowerLetter] + 1
-		if counter[lowerLetter] > 1 {
+
+		if seen[lowerLetter] {
 			return false
 		}
+		seen[lowerLetter] = true
 	}
 	return true
 }
