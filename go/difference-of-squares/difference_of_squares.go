7e0fb3e4836ae0640941818c2177e1f01c41cd8e
Prashanth Rao <pbrao@pmbrao.com>
2019-02-17 22:54:28 -0600

    Optimized algorithms

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -4,47 +4,22 @@ import (
 	"math"
 )
 
-// SquareOfSum ...
+// SquareOfSum calculates the square of a sum of numbers
 func SquareOfSum(n int) int {
 
-	result := 0
-	for i := 0; i <= n; i++ {
-		result = result + i
-	}
-	return int(math.Pow(float64(result), 2))
+	sum := n * (n + 1) / 2
+	return int(math.Pow(float64(sum), 2))
 }
 
-// SumOfSquares ...
+// SumOfSquares calculates the sum of a square of numbers
 func SumOfSquares(n int) int {
 
-	result := 0
-	for i := 0; i <= n; i++ {
-		result = result + int(math.Pow(float64(i), 2))
-	}
-	return result
+	return n * (n + 1) * (2*n + 1) / 6
 }
 
-// Difference ...
+// Difference calculates the difference between the square of sum and sum of squares
 func Difference(n int) int {
 
 	return SquareOfSum(n) - SumOfSquares(n)
 
 }
-
-/*
-$ go test -v --bench . --benchmem
-=== RUN   TestSquareOfSum
---- PASS: TestSquareOfSum (0.00s)
-=== RUN   TestSumOfSquares
---- PASS: TestSumOfSquares (0.00s)
-=== RUN   TestDifference
---- PASS: TestDifference (0.00s)
-goos: linux
-goarch: amd64
-pkg: github.com/pbrao/exercism/go/difference-of-squares
-BenchmarkSquareOfSum-4          20000000                77.4 ns/op             0 B/op             0 allocs/op
-BenchmarkSumOfSquares-4           500000              3898 ns/op               0 B/op             0 allocs/op
-BenchmarkDifference-4             300000              3544 ns/op               0 B/op             0 allocs/op
-PASS
-ok      github.com/pbrao/exercism/go/difference-of-squares      4.758s
-*/
